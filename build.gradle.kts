36d11c819fa9219f2f2a37a8a1ccd0392a0909cc
epoll-reactor <>
2022-03-17 12:58:04 +0100

    Install dependencies and make simple routing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-html-builder:1.6.7")
+    implementation("io.ktor:ktor-freemarker:1.6.7")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,9 @@
 package com.example
 
+import io.ktor.freemarker.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,6 +11,9 @@ import io.ktor.server.routing.*
 fun main() {
     embeddedServer(Netty, port = 8080) {
         routing {
+            static("/static") {
+                resources("files")
+            }
             get ("/") {
                 call.respondText("Bonnie & Clyde")
             }
