49c74ad6e478eb84bf7f2fd855dc7a8fc9305fec
epoll-reactor <>
2022-04-10 15:20:30 +0200

    Get rid of unneeded data class in dialog.

--- src/main/kotlin/com/weak_project/views/ErrorDialog.kt
+++ src/main/kotlin/com/weak_project/views/ErrorDialog.kt
@@ -5,21 +5,15 @@ import io.ktor.freemarker.*
 import io.ktor.response.*
 
 /**
- * Dialog view. Used by FreeMarker.
- */
-data class Dialog(val info: String)
-
-/**
  * Shows page with given message.
  *
  * @TODO floating (pop-up) window.
  */
 suspend fun ApplicationCall.respondErrorDialog(message: String) {
-    val dialog = Dialog(message)
     respond(
         FreeMarkerContent(
             "src/main/resources/templates/ErrorDialog.html",
-            mapOf("dialog" to dialog)
+            mapOf("dialog" to message)
         )
     )
 }
